3b0c08b0e10b52b1a3cd7bbec0dc1b0c7be1a1fd
migueldavid006 <migueldavid006@hotmail.com>
2023-01-07 09:31:50 -0500

    enviroment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+// import { API_URL } from "../env";
+
+import { API_URL } from "../env";
 
 function App() {
 
   return (
-  <div>hello world</div>
-  )
+    <div>
+      <h1>hello world {API_URL}</h1>
+    </div>
+  );
 }
 
 export default App
